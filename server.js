1f2e65f0c438b288bf912711470e2ae8339b50a0
Aseptrisna <Aseptrisna1995@gmail.com>
2022-12-10 09:13:48 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { randomFillSync } = require('crypto');
+const fs = require('fs');
+const http = require('http');
+const os = require('os');
+const path = require('path');
+const busboy = require('busboy');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+
+const random = (() => {
+  const buf = Buffer.alloc(16);
+  return () => randomFillSync(buf).toString('hex');
+})();
+
+const succes_response ={
+    code:200,
+    status:true,
+    message:"Berhasil Simpan Video" 
+}
+
+const error_response ={
+    code:200,
+    status:true,
+    message:"Gagal Simpan Video" 
+}
+
+app.use(express.static("static"));
+app.use(express.static(process.env.FILE));
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use((req, res) => {
+  if (req.method === 'POST') {
+    const bb = busboy({ headers: req.headers });
+    bb.on('file', (name, file, info) => {
+        const { filename, encoding, mimeType } = info;
+      const saveTo = path.join(process.env.FILE,filename );
+      file.pipe(fs.createWriteStream(saveTo));
+    });
+    bb.on('close', () => {
+      res.writeHead(200, { 'Connection': 'close' });
+      res.end(JSON.stringify(succes_response));
+    });
+    req.pipe(bb);
+    return;
+  }
+  res.writeHead(404);
+  res.end(JSON.stringify(error_response));
+}).listen(process.env.PORT, () => {
+  console.log('Listening for requests:'+process.env.PORT);
+});
