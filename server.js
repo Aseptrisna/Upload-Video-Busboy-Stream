2185c1ccc9ba4319dadd2714cb7640bf907234de
Aseptrisna <Aseptrisna1995@gmail.com>
2022-12-12 10:13:36 +0700

    set limit

--- server.js
+++ server.js
@@ -38,20 +38,37 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use((req, res) => {
+  const limit_reach = false;
+  const limit_reach_err = "File is too large!";
+  console.log("Uploading video...");
   if (req.method === 'POST') {
-    const bb = busboy({ headers: req.headers, highWaterMark: 100 * 1024 * 1024 });
+    const bb = busboy({ headers: req.headers, highWaterMark: 100000 * 1024 * 1024 });
     bb.on('file', (name, file, info) => {
       const { filename, encoding, mimeType } = info;
       const saveTo = path.join(process.env.FILE, filename);
       file.pipe(fs.createWriteStream(saveTo));
     });
     bb.on('close', () => {
+      console.log("Uploading video Succes...");
       res.writeHead(200, { 'Connection': 'close' });
       res.end(JSON.stringify(succes_response));
     });
     req.pipe(bb);
     return;
   }
+  bb.on('limit', function(){
+    fs.unlink(process.env.FILE, function(){ 
+      limit_reach = true;
+      res.status(455).send(limit_reach_err);
+      res.end(JSON.stringify(error_response));
+     });
+   });
+   bb.on('finish', function() {
+    if(!limit_reach){
+      res.send("Image saved successfully!");
+    }
+  });
+  console.log("Uploading video gagal...");
   res.writeHead(404);
   res.end(JSON.stringify(error_response));
 }).listen(process.env.PORT, () => {
